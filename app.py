55e7c816bc153e0fb2bd8e44530cbcf8f2e4abc8
KasaiYuki <shreyas.seethalla@gmail.com>
2019-01-21 22:19:03 -0500

    error handling

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template, request
 import phalculator
 import WolframAlpha
 
+
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
