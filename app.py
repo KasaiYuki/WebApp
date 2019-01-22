f46651c13ff6e92dfc38297b13f23929ce5115d3
KasaiYuki <shreyas.seethalla@gmail.com>
2019-01-21 22:04:54 -0500

    finished v1

--- app.py
+++ app.py
@@ -2,31 +2,43 @@ from flask import Flask, render_template, request
 import phalculator
 import WolframAlpha
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
+
+@app.route('/kinematics')
+def kinematics():
+    return render_template('kinematics.html')
+
+
+@app.route('/forces')
+def forces():
+    return render_template('forces.html')
+
+
 @app.route('/wolframAsk')
 def wolframAsk():
-
     return render_template('wolframAsk.html')
+
 
 @app.route('/wolfram')
 def wolfram():
     response = WolframAlpha.getResponse(request.args.get('response'))
-    return(render_template('wolfram.html', html_response = response))
+    return (render_template('wolfram.html', html_response=response))
+
 
 @app.route('/phalculatorAsk')
 def phalculatorAsk():
-    return(render_template('phalculatorAsk.html'))
+    return (render_template('phalculatorAsk.html'))
 
 
 @app.route('/dPos', methods=['GET'])
 def dPos():
-    return(render_template('dPos.html'))
+    return (render_template('dPos.html'))
 
 
 @app.route('/dPos', methods=['GET', 'POST'])
@@ -34,23 +46,67 @@ def dPosA():
     time = int(request.form['time'])
     accel = int(request.form['accel'])
     iVel = int(request.form['iVel'])
-    return(render_template('dPos.html', filled = 'true', answer = phalculator.dPos(time, accel, iVel)))
-
-#mapping for myprofile
-@app.route('/addprofileform')
-def addprofileform():
-    return render_template('MyProfileForm.html')
+    return (render_template('dPos.html', filled='true', answer=phalculator.dPos(time, accel, iVel)))
 
 
+@app.route('/fVel', methods=['GET'])
+def fVel():
+    return (render_template('fVel.html'))
 
-#mapping for /addprofile
-@app.route('/addprofile')
-def addprofile():
-    return render_template('MyProfile.html', html_myname = request.args.get('myname'), html_state_of_residence = request.args.get('state_of_residence'), html_fav_subject = request.args.get('favorite_subject'))
+
+@app.route('/fVel', methods=['GET', 'POST'])
+def fVelA():
+    iVel = int(request.form['iVel'])
+    accel = int(request.form['accel'])
+    iPos = int(request.form['iPos'])
+    fPos = int(request.form['fPos'])
+    return (render_template('fVel.html', filled='true', answer=phalculator.fVel(iVel, accel, fPos, iPos)))
+
+
+@app.route('/combineComp', methods=['GET'])
+def combineComp():
+    return (render_template('combineComp.html'))
+
+
+@app.route('/combineComp', methods=['GET', 'POST'])
+def combineCompA():
+    vComp = int(request.form['vComp'])
+    hComp = int(request.form['hComp'])
+    return (render_template('combineComp.html', filled='true', answer1=phalculator.comp(2, vComp, hComp),
+                            answer2=phalculator.comp2(2, vComp, hComp)))
+
+
+@app.route('/splitVector', methods=['GET'])
+def splitVector():
+    return (render_template('splitVector.html'))
+
+
+@app.route('/splitVector', methods=['GET', 'POST'])
+def splitVectorA():
+    mag = float(request.form['mag'])
+    angle = float(request.form['angle'])
+    return (render_template('splitVector.html', filled='true', answer1=phalculator.comp(1, mag, angle),
+                            answer2=phalculator.comp2(1, mag, angle)))
+
+@app.route('/friction', methods=['GET'])
+def friction():
+    return (render_template('friction.html'))
+
+
+@app.route('/friction', methods=['GET', 'POST'])
+def frictionA():
+    coef = float(request.form['coef'])
+    mass = float(request.form['mass'])
+    angle = float(request.form['angle'])
+    incline = str(request.form['inc'])
+    return (render_template('friction.html', filled='true', answer=phalculator.friction(coef, mass, angle, incline)))
+
+
 
 @app.route('/studyguides')
 def studyguides():
-    return(render_template('studyguides.html'))
+    return (render_template('studyguides.html'))
+
 
 if __name__ == '__main__':
     app.run()
--- phalculator.py
+++ phalculator.py
@@ -1,24 +1,45 @@
 import math
+
+
 def dPos(t, a, vx0):
     changeInPos = (vx0 * t) + (0.5 * a * (t ** 2))
     return changeInPos
 
 
 def fVel(vx0, a, x, x0):
-    finalVel = vx0 ** 2 + (2 * a * (x - x0))
+    finalVel = math.sqrt(vx0 ** 2 + (2 * a * (x - x0)))
     return finalVel
 
 def comp(solveFor, num1, num2):
     if(solveFor is 1):
-        return f"Vertical Component: {split(num1, num2)[0]}, " \
-               f"Horizontal Component: {split(num1, num2)}"
+        return split(num1, num2)[0] #v comp
     if(solveFor is 2):
-        return f"Magnitude: {condense(num1, num2)}[0]" \
-               f"Angle: {condense(num1, num2)}[1])"
+        return pythag(num1, num2) #magnitude
 
-def condense(hor, ver):
-    return (hor**2 + ver**2), math.degrees(math.atan(ver/hor))
+def comp2(solveFor, num1, num2):
+    if(solveFor is 1):
+        return split(num1, num2)[1] #h comp
+    if(solveFor is 2):
+        return angle(num1, num2) #angle
 
-def split(angle, mag):
-    return (mag * math.degrees(math.sin(angle))), \
-           (mag * math.degrees(math.cos(angle)))
+def pythag(ver, hor):
+    return round(math.sqrt(hor**2 + ver**2),2)
+
+def angle(ver, hor):
+    return round(math.degrees(math.atan(ver / hor)),2)
+
+
+def split(mag, angle):
+    rads = math.radians(angle)
+    vcomp = round(math.sin(rads),2)
+    hcomp = round(math.cos(rads),2)
+    return mag*vcomp, mag*hcomp
+
+def friction(coef: float, mass: float, angle: float, inc):
+    fric = coef * (mass * 10)
+    if(angle is not 0):
+        if(inc == "l"):
+            fric *= math.sin(math.radians(angle))
+        elif(inc == "r"):
+            fric *= math.cos(math.radians(angle))
+    return round(fric,2)
