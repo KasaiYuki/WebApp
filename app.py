7a034c3b8e6129e7548da3d0e2159c073607b704
KasaiYuki <shreyas.seethalla@gmail.com>
2019-01-21 23:04:15 -0500

    trying port forwarding-not working :(

--- app.py
+++ app.py
@@ -5,13 +5,6 @@ import WolframAlpha
 
 app = Flask(__name__)
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
@@ -117,4 +110,4 @@ def studyguides():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
